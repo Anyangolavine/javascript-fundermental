d767e43e0858a4a7d479487f568bba8d74ddec8a
unknown <lavinemasheti@gmail.com>
2022-10-15 06:41:28 -0700

    first commit

---
+++ literal.js
@@ -0,0 +1,16 @@
+// '1 counted='  { 45+24}' sheep'
+console.log('1 counted $ {45+35}sheep');
+// let firstName='lavine';
+let lastName='masheti';
+// let fullName=firstName + +' 'lastName;
+console.log(fullName)
+let fullName =' lavine + + masheti';
+let firstName =' ${firstName} ${lastName}';
+let totalone=500;
+let totaltwo=400;
+let total= ' $ {totalone+totaltwo} ';
+console.log(total);
+let item ='cowpeace';
+let peace=1.67;
+let quantity = 4;
+console.log(cowpeace);
---
+++ strings.js
@@ -0,0 +1,13 @@
+let annoyingLaugh = 'teehee so funny! teehee!';
+// annoyingLaugh.replace('teehee', 'haha')
+console.log(annoyingLaugh);
+console.log(annoyingLaugh.replace('teehee','haha'));
+console.log(annoyingLaugh.replace('teehee','haha'));
+const age="5"+"4";
+console.log(age);
+console.log("pecan pie"[7])
+//new line
+console.log('hello\nworld');
+console.log('hello\'world\'');
+console.log('hello\nworld\nkenya');
+console.log('hello\rworld\'');
